9cf806230b6161a84385561f57a34f7675cfc22c
Kev-PC\Kev <kevinfb92@gmail.com>
2017-06-02 22:21:02 +0200

    express fix

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', './views');
 app.set('view engine', 'ejs');
 
 
