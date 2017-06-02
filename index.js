b8ac3f3a616aec77753db82ede939ef3d1b4ece4
Kev-PC\Kev <kevinfb92@gmail.com>
2017-06-02 14:48:47 +0200

    chat

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(request, response){
+	response.render('pages/index');
+});
+
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(app.get('port'), function(){
+	console.log('listening on *'+ app.get('port'));
+});
+
+
+/*var express = require('express');
+var app = express();
 var cool = require('cool-ascii-faces');
 var pg = require('pg');
+var io = require('socket.io');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -31,8 +61,6 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
-
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
@@ -44,3 +72,4 @@ app.get('/db', function (request, response) {
     });
   });
 });
+*/
